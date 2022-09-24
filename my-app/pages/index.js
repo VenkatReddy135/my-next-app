b01489843d008c780c117f6ef90c03b819bb8856
venkat <venkat.vv564@gmail.com>
2022-09-24 16:49:56 +0530

    Third Commit

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -15,7 +15,7 @@ export default function Home(props) {
 
 export const getStaticProps = async () => {
   const data = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=6"
+    "https://jsonplaceholder.typicode.com/posts?_limit=8"
   );
   const articles = await data.json();
 
