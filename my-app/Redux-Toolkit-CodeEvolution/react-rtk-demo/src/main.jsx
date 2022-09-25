dce35fc044d9fef29d23ac56dbb703d8b7f029ea
VenkatReddy135 <57211176+VenkatReddy135@users.noreply.github.com>
2022-09-25 17:32:04 +0530

    Adding Interceptors

--- my-app/Redux-Toolkit-CodeEvolution/react-rtk-demo/src/main.jsx
+++ my-app/Redux-Toolkit-CodeEvolution/react-rtk-demo/src/main.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+// import axios from 'axios'
 import { Provider } from 'react-redux'
 import store from './app/store'
 import './index.css'
 import App from './App'
+
+// axios.interceptors.request.use((request)=>{
+//   console.log(request)
+//   request.headers.channelName='venkat'
+//   return request
+// })
+
+// axios.interceptors.response.use((response)=>{
+//   console.log(response)
+  
+// })
 
 ReactDOM.render(
   <React.StrictMode>
